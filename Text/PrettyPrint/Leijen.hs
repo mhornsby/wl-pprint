17b9ea525ca21caf04b33f071431721aaba16e77
Víctor López Juan <victor@lopezjuan.com>
2018-03-31 12:21:11 +0200

    Move `OverlappingInstances` from `.cabal` file to Haskell source file
    This language extension is deprecated, we should eventually do without
    it.

--- Text/PrettyPrint/Leijen.hs
+++ Text/PrettyPrint/Leijen.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE OverlappingInstances #-}
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Text.PrettyPrint.Leijen
